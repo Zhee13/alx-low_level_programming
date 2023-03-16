ce147734b4d139306c8999bba76072288f1649ec
Zainab <olaidezainab4@gmail.com>
2023-03-16 08:58:30 -0700

    programming is starting to feel real

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ *Return: when it is successful
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
