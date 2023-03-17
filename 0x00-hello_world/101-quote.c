30dffe1ca4a3b3bd491899b572e5a4446f150553
Zainab <olaidezainab4@gmail.com>
2023-03-16 21:20:43 -0700

    unistd

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - main content
+ *
+ *Return: when successful,return at 1
+ */
+
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout");
+	return (1);
+}	
