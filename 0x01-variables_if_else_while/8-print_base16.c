9df8b508e2fb11414f40a3139072ecb42f414f2d
Zainab <olaidezainab4@gmail.com>
2023-03-19 04:47:32 -0700

    hexadecimals

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - To print hexadecimal numbers in lowercase
+ *
+ * Return: when successful at 0
+ */
+
+int main(void)
+{
+	int i;
+	char j;
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i + '0');
+	for (j = 'a' ; j <= 'f' ; j++)
+		putchar(j);
+	putchar('\n');
+	return (0);
+}
