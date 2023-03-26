e1d7014288f254ea3825884785e62fd7aab77797
Zainab <olaidezainab4@gmail.com>
2023-03-26 03:56:06 -0700

    numbers ten times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - to print numbers ten times
+ *
+ * Return: the number
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 1 ; i <= 10 ; i++)
+	{
+		for (j = 0 ; j <= 14 ; j++)
+		{
+			_putchar(j + '0');
+		}
+		_putchar('\n');
+	}
+}
