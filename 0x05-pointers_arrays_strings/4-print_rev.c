8c40b6bf83a789a120693f2b8de9993ebab22fa4
Zainab <olaidezainab4@gmail.com>
2023-03-28 14:58:43 -0700

    strings and arrays

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,11 +10,11 @@
 void print_rev(char *s)
 {
 	int i;
-	int count = 0;
+	int length = 0;
 
 	for (i = 0 ; s[i] != '\0' ; i++)
-		count++;
-	for (i = count ; i >= 0 ; i--)
+		length++;
+	for (i = length ; i >= 0 ; i--)
 		_putchar(s[i]);
 	_putchar('\n');
 }
