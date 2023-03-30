d0617e9aa4356cce3f0221cbeb25ead39fb2a807
Zainab <olaidezainab4@gmail.com>
2023-03-30 08:02:20 -0700

    arrays

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+#include <stdio.h>
+int _putchar(char c);
+char *cap_string(char *);
+char *string_toupper(char *);
+void reverse_array(int *a, int n);
+int _strcmp(char *s1, char *s2);
+char *_strncpy(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, int n);
+char *_strcat(char *dest, char *src);
+#endif
